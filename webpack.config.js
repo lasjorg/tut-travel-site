4301dc253a8ffa4f3ac98aae8b59ed65a6823c85
lasjorg <28780271+lasjorg@users.noreply.github.com>
2018-03-28 22:21:36 +0200

    Add babel:
    New webpack syntax, left in old syntax commented out with info links.

--- webpack.config.js
+++ webpack.config.js
@@ -5,5 +5,26 @@ module.exports = {
   output: {
     path: path.resolve(__dirname, './app/temp/scripts'),
     filename: 'App.js'
+  },
+  module: {
+    rules: [
+      { test: /\.js$/, exclude: /node_modules/, loader: "babel-loader" }
+    ]
   }
+  /* https://babeljs.io/docs/setup
+   * https://webpack.js.org/guides/migrating/#module-loaders-is-now-module-rules
+   * https://webpack.js.org/guides/migrating/#what-are-options-
+  */
+/*   module: {
+    loaders: [
+      {
+        loader: 'babel-loader',
+        query: {
+          presets: ['es2015']
+        },
+        test: /\.js$/,
+        exclude: /node_modules/
+      }
+    ]
+  } */
 }
